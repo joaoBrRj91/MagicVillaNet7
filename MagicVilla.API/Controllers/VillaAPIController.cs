35b524da9e432b2e8569a5375bc42373e45d27f7
João Nascimento <joaonascimento@MacBook-Air-de-Joao.local>
2023-06-26 12:37:38 -0300

    ProducesResponseType

--- MagicVilla.API/Controllers/VillaAPIController.cs
+++ MagicVilla.API/Controllers/VillaAPIController.cs
@@ -13,6 +13,7 @@ namespace MagicVilla.API.Controllers
 
         [HttpGet]
         [Route("villas")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<VillaDto>))]
         public ActionResult<IEnumerable<VillaDto>> GetVillas()
             => Ok(VillaStore.VillaStorage.ToListVillaDto());
 
@@ -20,15 +21,18 @@ namespace MagicVilla.API.Controllers
 
         [HttpGet]
         [Route("villa")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(VillaDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDto> GetVilla(string codVilla)
         {
 
             var villa = VillaStore.VillaStorage.Where(n => n.CodVilla! == codVilla).FirstOrDefault();
 
-            if (villa is not null)
-                return Ok(villa.VillaToVillaDto());
+            if (villa is null)
+                return NotFound();
 
-            return NotFound();
+            return Ok(villa.VillaToVillaDto());
+
         }
 
     }
