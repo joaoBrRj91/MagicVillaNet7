23538507d12945cfb0467783ada1379e07c0c9f6
João Nascimento <joaonascimento@MacBook-Air-de-Joao.local>
2023-06-28 16:31:10 -0300

    Create Class Response for Api

---
+++ MagicVilla.API/Models/Response/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using MagicVilla.API.Models.Response.Interface;
+
+namespace MagicVilla.API.Models.Response
+{
+	public class ApiResponse : IResponse
+    {
+        public ApiResponse(HttpStatusCode statusCode, string statusCodeMessage, List<string> errorMessages, object result)
+        {
+            StatusCode = statusCode;
+            StatusCodeMessage = statusCodeMessage;
+            ErrorMessages = errorMessages;
+            Result = result;
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string StatusCodeMessage { get; set; }
+
+        public List<string> ErrorMessages { get; set; }
+
+        public object Result { get; set; }
+    }
+}
+
---
+++ MagicVilla.API/Models/Response/Interface/IResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MagicVilla.API.Models.Response.Interface
+{
+	public interface IResponse
+	{
+        public List<string> ErrorMessages { get; set; }
+
+        public object Result { get; set; }
+    }
+}
+
